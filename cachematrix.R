08c09bb13333292c55cf4de12588680dcfda61d3
jogaudard <joseph.gaudard@pm.me>
2020-02-19 19:57:54 +0100

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The idea here is to calculate the inverse matrix only the first time, and then to get the inversce matrix from the cache. It is apparently much faster in case of big matrix.
 
-## Write a short comment describing this function
+## the makeCacheMatrix creates a list of fonctions setting and getting the content of x and the inverse of x
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invx <- NULL
+        set <- function(y) {
+                x <<- y
+                invx <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) invx <<- inv
+        getinv <- function() invx
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+
+## The first time cacheSolve is run, it calculates the inverse of the matrix (using Solve) and store it in the cache (setinv()). Then it retrieves the value of invx from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invx <- x$getinv()
+        if(!is.null(invx)) {
+                message("getting cached data")
+                return(invx)
+        }
+        data <- x$get()
+        invx <- solve(data, ...)
+        x$setinv(invx)
+        invx
 }
+
